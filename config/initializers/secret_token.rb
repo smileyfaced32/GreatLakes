580c5bc680a968b346521d27010e16d302050ddf
smileyfaced32 <fiore.dennis@gmail.com>
2011-01-25 08:27:31 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Greatlakes::Application.config.secret_token = 'facb7fbd1fe4bb7940f816a336828d124355948a939b093ab6ef915ebdc5732e49575d61c68975dc5f1d78e8a2c718ec3695919b832068d8614fc3e06dd84ad4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Greatlakes::Application.config.session_store :cookie_store, :key => '_greatlakes_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Greatlakes::Application.config.session_store :active_record_store
